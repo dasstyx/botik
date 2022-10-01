af8d4a10c20743efeb1c4212b6c9a30252d02389
dasstyx <dasstyx@gmail.com>
2022-10-02 02:20:52 +0300

    move message handlers to folder

--- src/app/tg_app.py
+++ src/app/tg_app.py
@@ -1,48 +1,11 @@
-import telebot
-
 from src.app.app import App
-from src.app.raw_message_handlers import RawMessageHandlers
+from src.app.message_handlers.tg_raw_message_handlers import TgRawMessageHandlers
 from src.app.user_input import UserInput
 from src.communication.api import TgApi
 from src.navigation.navigation import Navigation
 from src.page.page_factory import PageFactory, TgPageFactory
 
 import asyncio
-
-
-class TgRawMessageHandlers(RawMessageHandlers):
-    def _initialize_handlers(self, bot):
-        bot.message_handler(content_types=['text'])(self.message_reply)
-        bot.message_handler(content_types=['contact'])(self.phone_reply)
-        bot.message_handler(content_types=['location'])(self.location_reply)
-        bot.message_handler(commands=['start'])(self.start_reply)
-
-        bot.callback_query_handler(func=lambda call: True)(self.callbacks_handle)
-
-    async def _get_user_from_message(self, message):
-        user_id = message.from_user.id
-        return await self._get_user_from_id(user_id)
-
-    async def callbacks_handle(self, call):
-        data = call.data
-
-        user_id = call.from_user.id
-        user = await self._get_user_from_id(user_id)
-
-        await self.user_input.forward_inline_button(user, data)
-
-    async def location_reply(self, message):
-        user = await self._get_user_from_message(message)
-        location = message.location
-
-        await user.storage.add_entry("location", location)
-
-    async def phone_reply(self, message):
-        user = await self._get_user_from_message(message)
-        number = message.contact.phone_number
-        print(f"Got a number! {number}")
-
-        await user.storage.add_entry("phone", number)
 
 
 class TgApp(App):
