0d2f58fc8a16844e0b7baef95801d6121645e337
dasstyx <dasstyx@gmail.com>
2022-10-29 01:21:09 +0300

    move test pages to folder

--- testapp/pages_data.py
+++ testapp/pages_data.py
@@ -1,9 +1,9 @@
 from src.page.page_data import PageData
-from testapp.info_page import InfoPage
-from testapp.main_page import MainPage
-from testapp.native_args_page import NativeArgsPage
-from testapp.phone_page import PhonePage
-from testapp.stub_page import StubPage
+from testapp.pages.info_page import InfoPage
+from testapp.pages.main_page import MainPage
+from testapp.pages.native_args_page import NativeArgsPage
+from testapp.pages.phone_page import PhonePage
+from testapp.pages.stub_page import StubPage
 
 main_data = PageData(MainPage, '/', '', inline=True, one_time=True)
 info_data = PageData(InfoPage, '/info', main_data, inline=False, one_time=False)
